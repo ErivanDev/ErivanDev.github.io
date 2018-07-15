329f94fba0aa35099b71ef82adc7b7d4dbcceff9
ErivanPereira <34924779+ErivanPereira@users.noreply.github.com>
2018-07-14 21:51:41 -0300

    Add files via upload

--- script/pontos.js
+++ script/pontos.js
@@ -1,72 +1,72 @@
 var icons = [];
 var person;
+var mouseX = 0;
+var mouseY = 0;
 
 function setup(){
-    var canvas = createCanvas(325, 500);
+    var canvas = createCanvas(1200,400);
     canvas.parent("canvas");
     canvas.id("banner-canvas");
-    document.getElementById("banner-canvas").style.width = "100%";
-    document.getElementById("banner-canvas").style.height = "100%";
-    var requestURL = 'script/new.json';
+    var b = $("#canvas").outerWidth();
+    $("#banner-canvas").css( { width: b , height: b/3 } );
+
+    var requestURL = 'script/passaro.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
-    grupo = [];
-    for(var i=0;i<255;i++) grupo.push(new group(i)); 
+    g = [];
+    // for(var i=0;i<255;i++) grupo.push(new group(i)); 
     request.onload = function(){
-        var p = request.response;
-        for(var i = 0; i<p.length; i++) grupo[p[i].cor].children.push(new points(p[i].x*5,p[i].y*5,p[i].cor));
+        var p = request.response; var i; var s = p.length; 
+        for(i=0;i<s;i++){
+            let o = { x: p[i].x, y: p[i].y, r: p[i].red, g: p[i].green, b: p[i].blue, h: p[i].x, v: p[i].y  };
+            g.push(o);
+        }
     }
-    noStroke();
+    // noStroke();
 }
 
 function draw(){
     background(255);
-    for(var i=0; i<grupo.length; i++) grupo[i].update();
-    //console.log(cont);
-}
 
-function group(cor){
-    this.children = [];
-    this.cor = cor;
-    this.update = function(){
-        fill(this.cor);
-        for(var i=0;i<this.children.length;i++){
-            this.children[i].update();
-        }
-    }
-}
+    var b = $("#canvas").outerWidth();
+    $("#banner-canvas").css( { width: b , height: b/3 } );
+ 
+    var img = createImage(1200, 400);
+    img.loadPixels();
 
-function points(x,y,cor){
-    this.x = x;
-    this.y = y;
-    this.cor = cor;
-    this.xinicial = x;
-    this.yinicial = y;
-    this.update = function(){
-        //cont++;
-        var distancia = dist(mouseX,mouseY,this.x,this.y);
-        if(distancia <=50){ 
-            var t = coordenadasTan(this.x,this.y);
-            this.x -= Math.cos(t) * (100 - distancia)/2; 
-            this.y -= Math.sin(t) * (100 - distancia)/2;
+    var i; var s = g.length;
+    for(i=0; i<s; i++){
+        var o = g[i];
+        var e = d(mouseX,mouseY,o.x,o.y);
+        if(e <=50){ 
+            var t = c(o.x,o.y);
+            o.x -= Math.cos(t) * (50 - e)/2; 
+            o.y -= Math.sin(t) * (50 - e)/2;
         }
         else{
-            this.x = (this.x + this.xinicial)/2; 
-            this.y = (this.y + this.yinicial)/2; 
-        }
-        rect(this.x,this.y,5,5);
+            o.x = (o.x + o.h)/2; 
+            o.y = (o.y + o.v)/2; 
+        }            
+        img.set(o.x, o.y, [o.r, o.g, o.b, 255]); 
     }
+
+    img.updatePixels();
+    image(img, 0, 0);
+    // console.log(img);
 }
 
-function coordenadasTan(x,y){
-    var yy = mouseY-y;
-    var xx = mouseX-x;
-    if (xx != 0){
-        t = Math.atan(yy/xx);
-        if (xx<0) t+=PI;
-        else if (xx>0 && yy<0) t+=TWO_PI;
+function d(x1,y1,x2,y2){
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function c(x,y){
+    var v = mouseY-y; var h = mouseX-x; var t = 0;
+    if (h != 0){
+        t = Math.atan(v/h);
+        if (h<0) t+=Math.PI;
+        else if (h>0 && v<0) t+=Math.PI*2;
     }
     return t;
 }
